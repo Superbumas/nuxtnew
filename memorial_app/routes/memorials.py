dbbb1fc62a502641ee015e37da5a049c7a136507
Andrius <wimass@gmail.com>
2024-09-14 20:50:48 +0100

    s

--- memorial_app/routes/memorials.py
+++ memorial_app/routes/memorials.py
@@ -1,30 +1,42 @@
-from flask import Blueprint, request, jsonify
-from models import MemorialProfile
-from extensions import db
-
-bp = Blueprint('memorials', __name__)
+from flask import request, jsonify
+from werkzeug.utils import secure_filename
+import os
+from memorial_app import db
+from memorial_app.models import MemorialProfile
 
 @bp.route('/api/memorials', methods=['POST'])
 def create_memorial():
-    data = request.get_json()
+    data = request.form
+    files = request.files
+
+    # Save profile photo
+    profile_photo = files.get('profile_photo_url')
+    if profile_photo:
+        profile_photo_filename = secure_filename(profile_photo.filename)
+        profile_photo.save(os.path.join('uploads', profile_photo_filename))
+    else:
+        profile_photo_filename = None
+
+    # Save cover photo
+    cover_photo = files.get('cover_photo_url')
+    if cover_photo:
+        cover_photo_filename = secure_filename(cover_photo.filename)
+        cover_photo.save(os.path.join('uploads', cover_photo_filename))
+    else:
+        cover_photo_filename = None
+
     new_profile = MemorialProfile(
-        name=data['name'],
-        birthdate=data['birthdate'],
-        deathdate=data['deathdate'],
-        biography=data['biography']
+        first_name=data.get('first_name'),
+        last_name=data.get('last_name'),
+        biography=data.get('biography'),
+        profile_photo_url=profile_photo_filename,
+        cover_photo_url=cover_photo_filename,
+        date_of_birth=data.get('date_of_birth'),
+        date_of_death=data.get('date_of_death'),
+        country=data.get('country'),
+        city=data.get('city'),
+        timeline_events=data.get('timeline_events')
     )
     db.session.add(new_profile)
     db.session.commit()
     return jsonify(new_profile.to_dict()), 201
-
-@bp.route('/api/memorials', methods=['GET'])
-def get_memorials():
-    profiles = MemorialProfile.query.all()
-    return jsonify([profile.to_dict() for profile in profiles])
-
-@bp.route('/api/memorials/<int:id>', methods=['DELETE'])
-def delete_memorial(id):
-    profile = MemorialProfile.query.get_or_404(id)
-    db.session.delete(profile)
-    db.session.commit()
-    return '', 204
