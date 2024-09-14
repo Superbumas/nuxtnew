49fd00c72bf12b61ad318356fbbee38b9286335e
Andrius <wimass@gmail.com>
2024-09-14 19:49:02 +0100

    s

--- memorial_app/models.py
+++ memorial_app/models.py
@@ -19,6 +19,7 @@ class Role(db.Model, RoleMixin):
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(80), unique=True, nullable=False)
     password_hash = db.Column(db.String(128))
     google_id = db.Column(db.String(128))
     facebook_id = db.Column(db.String(128))
