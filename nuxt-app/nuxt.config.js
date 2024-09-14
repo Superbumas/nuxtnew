e7450ff25057e815bec9ec3754f85a9250658c2d
Andrius <wimass@gmail.com>
2024-09-14 18:20:00 +0100

    s

--- nuxt-app/nuxt.config.js
+++ nuxt-app/nuxt.config.js
@@ -23,7 +23,7 @@ export default {
     '@nuxtjs/dotenv'
   ],
   axios: {
-    baseURL: 'http://0.0.0.0:5000/api' // Ensure the base URL matches your Flask API
+    baseURL: 'http://localhost:5000/api' // Ensure the base URL matches your Flask API
   },
   auth: {
     strategies: {
