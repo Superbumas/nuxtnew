39ddaa7487f6eb009b32b06af47e241093bf65ad
Andrius <wimass@gmail.com>
2024-09-14 17:44:46 +0100

    s

--- nuxt-app/plugins/auth.js
+++ nuxt-app/plugins/auth.js
@@ -1,12 +1,5 @@
 export default function ({ $auth }) {
-    if (!$auth) {
-      console.error('Auth module is not initialized')
-      return
-    }
-
-    $auth.onRedirect((to, from) => {
-      console.log('Redirecting from', from, 'to', to)
-      // you can optionally change `to` by returning a new value
-      return to
-    })
-  }
+  $auth.onError((error, name, endpoint) => {
+    console.error(name, error)
+  })
+}
