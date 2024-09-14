01054cd065d3671dffe0259a73e5dab9f39ecf38
Andrius <wimass@gmail.com>
2024-09-14 17:20:49 +0100

    s

--- memorial_app/extensions.py
+++ memorial_app/extensions.py
@@ -1,8 +1,11 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_cors import CORS
+from flask_mail import Mail
+from flask_security import Security
 
 db = SQLAlchemy()
 login = LoginManager()
-cors = CORS()
+cors = CORS()  # Initialize CORS
 mail = Mail()
 security = Security()
