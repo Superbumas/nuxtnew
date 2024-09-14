10c1c4a77b3c5f96b3a2b270b451a3beda99003f
Andrius <wimass@gmail.com>
2024-09-14 19:06:56 +0100

    s

--- nuxt-app/tailwind.config.js
+++ nuxt-app/tailwind.config.js
@@ -1,11 +1,14 @@
-module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+   // tailwind.config.js
+   module.exports = {
+    content: [
+      './pages/**/*.{vue,js}',
+      './components/**/*.{vue,js}',
+      './layouts/**/*.{vue,js}',
+      './plugins/**/*.{js,ts}',
+      './nuxt.config.{js,ts}'
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
