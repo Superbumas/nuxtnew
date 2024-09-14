0da488f3daad772da423fd2bc15132ca4a75b447
Andrius <wimass@gmail.com>
2024-09-14 21:05:22 +0100

    s

--- memorial_app/extensions.py
+++ memorial_app/extensions.py
@@ -4,6 +4,8 @@ from flask_cors import CORS
 from flask_mail import Mail
 from flask_security import Security
 from flask_migrate import Migrate
+from flask_wtf.csrf import CSRFProtect
+from flask_uploads import UploadSet, configure_uploads, IMAGES
 
 db = SQLAlchemy()
 login = LoginManager()
@@ -11,3 +13,6 @@ cors = CORS()
 mail = Mail()
 security = Security()
 migrate = Migrate()
+csrf = CSRFProtect()
+upload = UploadSet('uploads', IMAGES)
+
